ad2a363b46683df5503deb3e6b5ee601f574f32d
Daniel.Yu <adam741963@gmail.com>
2020-09-23 19:54:17 -0700

    route

---
+++ routes/api-routes.js
@@ -0,0 +1,9 @@
+const { json } = require('sequelize');
+
+const db = require('../models');
+
+module.exports = function(app) {
+    app.get('/api/spots', (req, res) => {
+        db.Spots.
+    })
+}
--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const app = express();
 
 const db = require('./models');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
-db.sequelize.sync({ forec: true }).then(() => {
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view-engine', 'handlebars');
+
+
+// db.sequelize.sync({ forec: true }).then(() => {
   app.listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`app listening on -> localhost:${PORT}`);
   });
-});
+// });
