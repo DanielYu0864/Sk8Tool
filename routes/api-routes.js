0aea325abc00a4408e74fde2e1d95b84d87beb16
Daniel.Yu <adam741963@gmail.com>
2020-09-23 20:30:03 -0700

    server with handlebars

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ const { json } = require('sequelize');
 const db = require('../models');
 
 module.exports = function(app) {
-    app.get('/api/spots', (req, res) => {
-        db.Spots.
-    })
+    // app.get('/api/spots', (req, res) => {
+    //     db.Spots.
+    // })
 }
