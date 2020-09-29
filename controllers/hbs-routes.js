7e92ca408ed31c4f49d7f1554a10fa30a01a81d6
Daniel.Yu <adam741963@gmail.com>
2020-09-28 17:07:10 -0700

    handlebars

--- controllers/hbs-routes.js
+++ controllers/hbs-routes.js
@@ -2,7 +2,17 @@ module.exports = function(app) {
     app.get('/', (req, res) => {
         res.render('index');
     });
+    app.get('/parks', (req, res) => {
+        res.render('parks');
+    });
+    app.get('/shops', (req, res) => {
+        res.render('shops');
+    });
 
+
+    app.get('/weather', (req, res) => {
+        res.render('weather');
+    });
 
 
 }
