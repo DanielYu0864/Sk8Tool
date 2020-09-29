65eee9a0e30fffdf5e7ccb1bb5364c1ed0835b99
Daniel.Yu <adam741963@gmail.com>
2020-09-29 16:53:03 -0700

    get latitude and longitude

--- public/assets/js/spots.js
+++ public/assets/js/spots.js
@@ -1,15 +1,59 @@
+const googleMapApiKey = "AIzaSyDmZhf4Cy3XVS_6hruDDGNfWfd0Uaxfxp4"; // D
+
 $(function(){
-    //creat-form is from activity code. need to rename
-    $('create-form').on('submit', function(event){
+    //*creat-form is from activity code. need to rename
+    //*function initMap(latitude, longitude) {
+    //*     const myLatLng = { lat: latitude, lng: longitude };
+    //*     const map = new google.maps.Map($('#current-spot'), {
+    //*       zoom: 4,
+    //*       center: myLatLng,
+    //*     });
+    //*     new google.maps.Marker({
+    //*       position: myLatLng,
+    //*       map,
+    //*       title: "Hello World!",
+    //*     });
+    //* }
+    let latitude;
+    let longitude;
+    function success(pos) {
+        var crd = pos.coords;
+
+        latitude = crd.latitude;
+        longitude = crd.longitude;
+        $('#current-spot').html(`<p class='latitude'>Latitude: ${latitude}</p><p class='longitude'>Longitude: ${longitude}</p>`);
+        //* var latlon = latitude + "," +  longitude;
+        //* var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=400x300&sensor=false&key=${googleMapApiKey}`;
+        //* $('#current-spot').html(`<img src='${img_url}'>`);
+
+        console.log('Your current position is:');
+        console.info(crd);
+        console.log(`Latitude : ${latitude}`);
+        console.log(`Longitude: ${longitude}`);
+        return latitude, longitude;
+    }
+    $('#use-current-location').on('click', (event) => {
+        event.preventDefault();
+
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success);
+          } else {
+            console.log("Geolocation is not supported by this browser.");
+          }
+    });
+    $('.create-form').on('submit', function(event){
         event.preventDefault();
 
         var newSpot ={
             name: $('#spot-name').val().trim(),
-            description: $('#spot_description').val().trim,
+            description: $('#spot-description').val().trim(),
             city: $('#spot-city').val().trim(),
-            crossStreets: $('#cross-street')
+            crossStreets: $('#cross-streets').val().trim(),
+            latitude: latitude,
+            longitude: longitude
         };
-
+        console.log(newSpot);
         $.ajax('api/spots',{
             type: 'POST',
             data: newSpot
@@ -19,15 +63,15 @@ $(function(){
         });
     });
     //this doesn't exist yet but I am coding it incase we want to add it
-    $('.delete-spot').on('click', function(event){
-        var id = $(this).data('id');
+    // $('.delete-spot').on('click', function(event){
+    //     var id = $(this).data('id');
 
-        $.ajax('/api/spots' + id, {
-            type: 'DELETE'
-        }).then(function(){
-            console.log('Spot deleted', id);
-            location.reload();
-        });
-    });
+    //     $.ajax('/api/spots' + id, {
+    //         type: 'DELETE'
+    //     }).then(function(){
+    //         console.log('Spot deleted', id);
+    //         location.reload();
+    //     });
+    // });
 
 });
