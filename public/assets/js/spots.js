e9a5bb24d468d4dd124d7df7cd4437af7bf5c50e
Daniel.Yu <adam741963@gmail.com>
2020-10-05 16:52:55 -0700

    api

--- public/assets/js/spots.js
+++ public/assets/js/spots.js
@@ -46,18 +46,18 @@ $(function(){
       }
   });
 //display yes security when input box if yes
-  $('#security-guards').on('click', function(event) {
+  $('#security-guards-yes').on('click', function(event) {
     event.preventDefault();
     let securityData = $(this).data('security');
-    if(securityData === true && securityGuards != true) {
+      if(securityData === true && securityGuards != true) {
         securityGuards = true;
         console.log('true');
         $('#security').append('<input class="security-when" id="securityWhen" sytle="display-none" placeholder="If yes when?">');
 
-      } else if(securityData === false && securityGuards != false){
+      } else {
         securityGuards = false;
         console.log('false');
-        $('#securityWhen').remove();
+        // $('#securityWhen').remove();
     }
   });
 // when the submit button 'click' will push the data to the database
